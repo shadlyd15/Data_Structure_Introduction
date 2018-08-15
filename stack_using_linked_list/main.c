59007450675d6c154e8760044508adb0acf009c0
shadlyd15 <shadlyd15@gmail.com>
2018-08-15 11:47:37 +0600

    Initial Commit

---
+++ stack_using_linked_list/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "debug.h"
+
+struct stack_t{
+	int data;
+	int top;
+};
+
+
+int main(int argc, char const *argv[]){
+	DEBUG_OK("Stack Using Linked List Example");
+	return 0;
+}
